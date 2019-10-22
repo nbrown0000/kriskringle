d9962b3936a089e797f13498afbf6a0d2a421071
nbrown0000 <brown.nathanjames@gmail.com>
2019-10-23 00:21:57 +1100

    Finished algorithm for generating pairings. Styled page to look cool.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
-import { Stack } from './Stack';
+// import { Stack } from './Stack';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: '',
-      result: ''
+      result: []
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -19,21 +19,23 @@ class App extends Component {
   }
 
   handleSubmit(event) {
-    // convert textarea input to array of name
-    const list = this.state.value.split("\n")
-
-    // push names onto a stack
-    var stack = new Stack();
-    list.forEach(item => stack.push(item))
-    console.log(stack.pop())
-    console.log(stack.pop())
-    console.log(stack.pop())
-
-
-    // console.log(list)
-    // console.log(list.length)
-    // console.log(Math.floor(Math.random() * list.length))
     
+    var output = [];
+    var list1 = this.state.value.split("\n")
+    var list2 = this.state.value.split("\n")
+
+    while(list1.length && list2.length) {
+      var random1 = Math.floor(Math.random() * list1.length)
+      var random2 = Math.floor(Math.random() * list2.length)
+
+      if(list1[random1] !== list2[random2]) {
+        const removed1 = list1.splice(random1,1)
+        const removed2 = list2.splice(random2,1)
+        output.push(`${removed1} => ${removed2}`)
+      }
+    }
+
+    this.setState({result: output})
     event.preventDefault();
   }
 
@@ -41,24 +43,28 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App content">
+      <div className="App">
 
-          <form onSubmit={this.handleSubmit} className="column">
-            <label>
-              <p>Enter names below:</p>
-              <textarea onChange={this.handleChange} cols="25" rows="20" />
-            </label>
-            <p><input type="submit" value="Submit" /></p>
-          </form>
+        <h1 className='column'>Kris Kringle generator</h1>
+        <h2 className='column'>Assign each of your guests a different Kris Kringle.</h2>
+      <div className='content'>
+        <form onSubmit={this.handleSubmit} className="column">
+          <label>
+            <p>Enter names below:</p>
+            <textarea onChange={this.handleChange} cols="25" rows="20" />
+          </label>
+          <p><input type="submit" value="Generate Pairings" /></p>
+        </form>
 
-          <div className="column">
-            <label>
-              <p>Results:</p>
-              <textarea value={this.state.result} onChange={() => true} cols="25" rows="20" />
-            </label>
-          </div>
-
-
+        <div className="column">
+          <label>
+            <p>Results:</p>
+            <div className='box'>
+              {this.state.result.map((item,i) => (<li key={i}>{item}</li>))}
+            </div>
+          </label>
+        </div>
+        </div>
         
       </div>
     );
