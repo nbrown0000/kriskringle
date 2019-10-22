4e136da9639f8775c826d338e9a61e444a8f2b32
nbrown0000 <brown.nathanjames@gmail.com>
2019-10-23 00:30:22 +1100

    Updates

---
+++ precache-manifest.6d5dbbf9955ef19e989aeb28941e0b4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602117867acac1c61c9eadd84529f232",
+    "url": "/kriskringle/index.html"
+  },
+  {
+    "revision": "5efdb656ac7b6476d977",
+    "url": "/kriskringle/static/css/main.25f56a0d.chunk.css"
+  },
+  {
+    "revision": "d4f9f8a4d1e37b660f05",
+    "url": "/kriskringle/static/js/2.758517c9.chunk.js"
+  },
+  {
+    "revision": "5efdb656ac7b6476d977",
+    "url": "/kriskringle/static/js/main.8dea4365.chunk.js"
+  },
+  {
+    "revision": "7c1b509e9711b17334ff",
+    "url": "/kriskringle/static/js/runtime-main.5444aeb1.js"
+  }
+]);
